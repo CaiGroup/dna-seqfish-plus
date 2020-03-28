b24424567c34947b4e728fe5eec8bebacc1aa4d3
Nico Pierson <nicogpt@gmail.com>
2020-03-27 18:52:35 -0700

    removed example and bfmatlab package

--- threshold/examples/example_Script_TimEmbryos_ThresholdByCh.m
+++
@@ -1,9 +0,0 @@
-load('G:\Tim\102219\Experiment\processedimages\pos0\preProcessedData-pos0-Tim-Embryos-102219-2019-10-30.mat');
-
-folderArray = 0:29;
-experimentDir = 'G:\Tim\102219\Experiment';
-experimentName = 'Tim-102219';
-position = 0;
-typedots = 'log';
-threshold = thresholdbych(experimentDir, experimentName, position, ...
-folderArray, typedots, I);
